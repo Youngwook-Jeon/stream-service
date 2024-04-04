9c178088f405171e9cf8392074925f978b085a75
Youngwook-Jeon <ela87@naver.com>
2024-04-05 01:03:34 +0900

    Show recommended users list at the sidebar

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -5,8 +5,9 @@ import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/use-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Toggle() {
+export const Toggle = () => {
   const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
   const label = collapsed ? "Expand" : "Collapse";
 
@@ -37,4 +38,13 @@ export default function Toggle() {
       )}
     </>
   );
-}
+};
+
+export const ToggleSkeleton = () => {
+  return (
+    <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
+      <Skeleton className="h-6 w-[100px]" />
+      <Skeleton className="h-6 w-6" />
+    </div>
+  );
+};
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,15 +1,30 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { useIsClient } from "usehooks-ts";
 
+import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+
+import { RecommendedSkeleton } from "./recommended";
+import { ToggleSkeleton } from "./toggle";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export default function Wrapper({ children }: WrapperProps) {
+  const isClient = useIsClient();
   const { collapsed } = useSidebar((state) => state);
+
+  if (!isClient) {
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
+  }
+
   return (
     <aside
       className={cn(
