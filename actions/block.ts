0fa3d90963e9acbce16c73d3824f5f52d72fd687
Youngwook-Jeon <ela87@naver.com>
2024-04-13 16:58:33 +0900

    Add error / 404 pages

--- actions/block.ts
+++ actions/block.ts
@@ -2,16 +2,33 @@
 
 import { revalidatePath } from "next/cache";
 
+import { getSelf } from "@/lib/auth-service";
 import { blockUser, unblockUser } from "@/lib/block-service";
+import { RoomServiceClient } from "livekit-server-sdk";
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
 
 export const onBlock = async (id: string) => {
-  const blockedUser = await blockUser(id);
-
-  revalidatePath("/");
-
-  if (blockedUser) {
-    revalidatePath(`/${blockedUser.blocked.username}`);
+  const self = await getSelf();
+  let blockedUser;
+  
+  try {
+    blockedUser = await blockUser(id);
+  } catch (error) {
+    // user is a guest
   }
+
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch (error) {
+    // user is not in the room
+  }
+  
+  revalidatePath(`/u/${self.username}/community`);
 
   return blockedUser;
 };
